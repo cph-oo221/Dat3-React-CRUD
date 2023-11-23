e8ae811ef135cfb0faa5fc01cfbbbbc6d8e4fe3f
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-11-23 10:15:29 +0100

    Create TodoFilter.jsx

---
+++ src/components/TodoFilter.jsx
@@ -0,0 +1,46 @@
+function TodoFilter({ children, todos, setTodos }) {
+  const handleSort = (e) => {
+    const option = e.target.value;
+    let sortedTodos;
+
+    switch (option) {
+      case "Id":
+        sortedTodos = [...todos].sort((a, b) => a.id - b.id);
+        break;
+      case "Note":
+        sortedTodos = [...todos].sort((a, b) => a.note.localeCompare(b.note));
+        break;
+      case "Due Date":
+        sortedTodos = [...todos].sort(
+          (a, b) => new Date(a.dueDate) - new Date(b.dueDate)
+        );
+        break;
+      case "Priority":
+        sortedTodos = [...todos].sort((a, b) =>
+          a.priority.localeCompare(b.priority)
+        );
+        break;
+      default:
+        sortedTodos = todos;
+    }
+
+    setTodos(sortedTodos);
+  };
+
+  return (
+    <div>
+      {children}
+      <label htmlFor="sort">Sort by:</label>
+      <select id="sort" onChange={handleSort}>
+        <option>Sort</option>
+        <option>Id</option>
+        <option>Note</option>
+        <option>Due Date</option>
+        <option>Priority</option>
+      </select>
+      {children}
+    </div>
+  );
+}
+
+export default TodoFilter;
